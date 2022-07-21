fad3568a1ad5005048cce27977bcccec20611d3d
treejw <42428487+treejw@users.noreply.github.com>
2022-07-21 17:42:32 +0900

    Create mjw.py

---
+++ week01/다리를_지나는_트럭/mjw.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    wait = deque(truck_weights)             # 대기중인 트럭
+    on_brdg = deque([0]*bridge_length)      # 다리 위 상태  (ex) bridge_length=4 → on_brdg=[0, 0, 0, 0]
+    on_brdg_weight = 0                      # 현재 다리 위에 있는 트럭 무게 총 합
+    
+    t = 0                                   # 현재 경과 시간
+    while wait or (on_brdg_weight > 0):
+        # 다리 위 - 맨 앞 제거
+        x = on_brdg.popleft()                             
+        on_brdg_weight -= x                      
+        
+        # 다리 위 - 맨 뒤 추가
+        if wait and (on_brdg_weight + wait[0] <= weight): 
+            new = wait.popleft()
+            on_brdg.append(new)
+            on_brdg_weight += new
+        else:
+            on_brdg.append(0)
+            
+        t += 1
+        
+    return t
