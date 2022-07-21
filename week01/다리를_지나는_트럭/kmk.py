ffa5c3898b6c25232d2413ad2f552b170457854c
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-07-21 17:43:55 +0900

    Add files via upload

---
+++ week01/다리를_지나는_트럭/kmk.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    
+    queue = [0]*bridge_length 
+    while queue:
+        answer += 1
+        queue.pop(0)
+        if truck_weights:
+            if sum(queue) + truck_weights[0] <= weight:
+                queue.append(truck_weights.pop(0))
+            else:
+                queue.append(0)
+    
+    return answer
+
