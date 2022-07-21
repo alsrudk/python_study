4f80d766aebaac9732e37b013bca9da46eb4d724
treejw <42428487+treejw@users.noreply.github.com>
2022-07-21 17:36:11 +0900

    Create mjw.py

---
+++ week01/소수찾기/mjw.py
@@ -0,0 +1,14 @@
+def find_Nprime(n):
+    cnt = 0
+    for i in range(2, n+1):
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0:
+                break
+        else:
+            cnt += 1
+            print( i)
+    return cnt
+
+def solution(n):
+    n = 30
+    return find_Nprime(n)
