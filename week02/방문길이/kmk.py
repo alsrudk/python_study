5e794bd0c8cbe7713a188a0d4f2c8e3dbd381777
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-07-28 17:29:59 +0900

    Update kmk.py

--- week02/방문길이/kmk.py
+++ week02/방문길이/kmk.py
@@ -1,19 +1,19 @@
 def solution(dirs):
     x,y = 0,0
-    answer = dict()
+    answer = set()
     
     for dir in dirs:
         if dir == 'U' and y < 5: 
-            answer[(x, y, x, y+1)] = True
+            answer.add((x, y, x, y+1))
             y += 1
         elif dir == 'D'and y > -5:
-            answer[(x, y-1, x, y)] = True
+            answer.add((x, y-1, x, y))
             y -= 1
         elif dir == 'L' and x > -5:
-            answer[(x-1, y, x, y)] = True
+            answer.add((x-1, y, x, y))
             x -= 1
         elif dir == 'R' and x < 5:
-            answer[(x, y, x+1, y)] = True
+            answer.add((x, y, x+1, y))
             x += 1
     
     return len(answer)
