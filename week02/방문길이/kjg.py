f9c27828c7b9ef942aa07524526f2c0819171489
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2022-07-29 13:05:11 +0900

    Create kjg.py

---
+++ week02/방문길이/kjg.py
@@ -0,0 +1,14 @@
+def solution(dirs):
+    move = {'U':(0,1), 'L':(-1,0), 'R':(1,0), 'D':(0, -1)}
+    visited = set()
+    
+    cur = (0,0)
+    for d in dirs:
+        mv = move[d]
+        nxt = (cur[0]+mv[0], cur[1]+mv[1])
+        if -5<=nxt[0]<=5 and -5<=nxt[1]<=5:
+            visited.add(tuple(sorted((cur,nxt)))) # ((1,0),(0,0)) -> ((0,0),(1,0))
+            cur = nxt
+            
+    return len(visited)
+    #지원쓰 코드
