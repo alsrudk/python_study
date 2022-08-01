126957bfe479d3a7a3a6dc0ee62693fe07ce791a
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-08-01 17:38:41 +0900

    Update kmk.py

--- week03/영어끝말잇기/kmk.py
+++ week03/영어끝말잇기/kmk.py
@@ -1,26 +1,20 @@
 def solution(n, words):
-    answer = []
-    previous = set()   #이전에 말한 단어들
+  previous = set()   #이전에 말한 단어들
     
-    turn = [0 for _ in range(n+1)]  #person마다 턴 수
-    person = 1 
+  turn = [0 for _ in range(n+1)]  #person마다 턴 수
+  person = 1 
     
-    for i in range(len(words)):
-        if words[i] in previous:  #이미 전에 말한 단어라면
-            turn[person] += 1
-            answer.append(person)
-            answer.append(turn[person])
-            return answer
-        elif words[i-1][-1] != words[i][0] and i != 0:  #끝말이 이어지지 않는다면
-            turn[person] += 1
-            answer.append(person)
-            answer.append(turn[person])
-            return answer
-        else:
-            previous.add(words[i]) 
-            turn[person] += 1
-            if person >= n:
-                person = 1
-            else:
-                person += 1
-    return [0,0]
+  for i in range(len(words)):
+    turn[person] += 1
+    if words[i] in previous: #이미 전에 말한 단어라면
+      return [person, turn[person])
+    elif words[i-1][-1] != words[i][0] and i != 0:  #끝말이 이어지지 않는다면
+      return [person, turn[person])
+    else:
+      previous.add(words[i])
+      if person >= n:
+        person = 1
+      else:
+        person += 1
+              
+  return [0,0]
