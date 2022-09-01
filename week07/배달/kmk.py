442af46cd2ee5e9c0aea8f6b162b9e37156884b2
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-09-01 14:16:00 +0900

    Create kmk.py

---
+++ week07/배달/kmk.py
@@ -0,0 +1,31 @@
+import heapq
+def dijkstra(start, graph, distance):
+    queue = []
+    heapq.heappush(queue, (0, start))
+    distance[start] = 0
+    while queue:
+        dist, now = heapq.heappop(queue)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(queue, (cost, i[0]))
+
+def solution(N, road, K):
+    answer = 0  
+    graph = [[] for _ in range(N+1)]
+    distance = [int(1e9)] * (N+1)
+    
+    for r in road:
+        a,b,c = r
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+        
+    dijkstra(1, graph, distance)
+    for d in distance:
+        if d <= K:
+            answer += 1
+
+    return answer
