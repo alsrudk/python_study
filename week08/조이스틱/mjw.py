f638cf49988678ac949898bdbfb0137e5d4f67a4
treejw <42428487+treejw@users.noreply.github.com>
2022-09-05 15:49:10 +0900

    Update mjw.py

--- week08/조이스틱/mjw.py
+++ week08/조이스틱/mjw.py
@@ -23,17 +23,15 @@ def search(names, visited, cursor, cnt, min_cnt=1e9):
         cur = cur-1 if cur > 0 else len(names)-1
         move += 1
     min_cnt = min(search(names, visited.copy(), cur, cnt+move, min_cnt), min_cnt)
-    
     return min_cnt
     
-def get_changeCnt(dest_ord):
-    tmp = abs(dest_ord - 0)
-    return min(tmp, 26-tmp)
+def get_changeCnt(x):
+    ordx = ord(x)-65
+    return min(ordx, 26-ordx)
 
 def solution(name):
     if set(name) == {'A'}:
         return 0
-    dest_name = list(map(lambda x:ord(x)-65, name)) # JAN -> [9,0,13]
-    names = [get_changeCnt(x) for x in dest_name]   # change_cnt
+    names = list(map(lambda x:get_changeCnt(x), name))
     visited = [0 if x > 0 else 1 for x in names]
     return search(names, visited, cursor=0, cnt=0, min_cnt=1e9)
