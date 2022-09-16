78e332b4463f06544141e1cdf8ab42fe23baffa9
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-09-16 19:14:53 +0900

    Create kmk.py

---
+++ week09/타겟넘버/kmk.py
@@ -0,0 +1,14 @@
+def dfs(idx, numbers, target, cur, result = [], answer = 0):
+    if idx == len(numbers):
+        if sum(cur) == target:
+            result.append(cur)
+        return
+    dfs(idx+1, numbers, target, cur+[numbers[idx]])
+    dfs(idx+1, numbers, target, cur+[-numbers[idx]])
+    
+    
+    return len(result)
+    
+    
+def solution(numbers, target):
+    return dfs(0, numbers, target, [])    
