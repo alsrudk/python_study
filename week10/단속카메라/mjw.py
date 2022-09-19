7d1fe74680255f7d0adb7866b3260e12e4e107e9
treejw <42428487+treejw@users.noreply.github.com>
2022-09-19 13:35:28 +0900

    Update mjw.py

--- week10/단속카메라/mjw.py
+++ week10/단속카메라/mjw.py
@@ -4,13 +4,13 @@ def solution(routes):
     routes.sort(key=lambda x: (x[0], -x[1])) # 진입 지점이 빠른 순 > 주행 거리 긴 순
     
     answer = 0
-    prev = routes[0]
+    prev = routes[0]    # 이전 카메라가 설치된 구간
     i = 1
     while i < len(routes):
         cur = routes[i]
         if prev[1] < cur[0]:
             answer += 1
-            prev = routes[i]
+            prev = cur
         else:
             prev = [max(prev[0], cur[0]), min(prev[1], cur[1])]
         i += 1
