015ad6b7da97baa284c69ddb3a6b259732dccb47
alsrudk <62791577+alsrudk@users.noreply.github.com>
2022-09-19 16:55:43 +0900

    Update kmk.py

--- week10/N개의_최소공배수/kmk.py
+++ week10/N개의_최소공배수/kmk.py
@@ -5,8 +5,6 @@ def get_lcm(num1, num2):
 def solution(arr):
     answer = 0
     for i in range(len(arr)-1):
-        print(arr[i], arr[i+1])
         answer = get_lcm(arr[i], arr[i+1])
         arr[i+1] = answer
-    
     return answer
